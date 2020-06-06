9a8b709c19619e9ac3484ecaa9a4841de3659eb9
Juan Manuel Medina <jmmedina@kodr.club>
2020-06-06 12:40:53 +0200

    Añado console.log

--- source/microservice-a/src/index.ts
+++ source/microservice-a/src/index.ts
@@ -9,6 +9,7 @@ export async function main(options: ApplicationConfig = {}) {
   await app.start();
 
   const url = app.restServer.url;
+  console.log("Ola ke ase");
   console.log(`Server is running at ${url}`);
   console.log(`Try ${url}/ping`);
 
